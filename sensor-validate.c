f6d3b7f54540f16a0be35d0fac9887a9a941bd29
varshaar24 <77425787+varshaar24@users.noreply.github.com>
2021-03-24 19:53:21 +0530

    Update sensor-validate.c

--- sensor-validate.c
+++ sensor-validate.c
@@ -5,14 +5,10 @@ int DiffIsLessThanOrEqualToMaxDelta(double value, double nextValue, double maxDe
   return (nextValue - value <= maxDelta);
 }
 
-int validateSOCreadings(double* values, int numOfValues,double maxDelta)
+int CheckSOCReadings(double* values, int numOfValues,double maxDelta)
 {
   int lastButOneIndex = numOfValues - 1;
-  if(values == 0)
-  {
-    return 1;
-  }
-
+  
   for(int i = 0; i < lastButOneIndex; i++)
   {
     if(DiffIsLessThanOrEqualToMaxDelta(values[i], values[i + 1], maxDelta))
@@ -24,4 +20,19 @@ int validateSOCreadings(double* values, int numOfValues,double maxDelta)
   
   return 1;
 }
+
+int validateSOCreadings(double* values, int numOfValues,double maxDelta)
+{
+  int RetVal=0;
+  if(values == 0)
+  {
+    return 1;
+  }
+  
+  RetVal = CheckSOCReadings(values, numOfValues,maxDelta);
+ 
+  return RetVal;
+}
+
+
 
