8ecf459ea7154038edcdce7d6856f3394391c2f9
varshaar24 <77425787+varshaar24@users.noreply.github.com>
2021-03-25 17:19:04 +0530

    Update test-validationtool.cpp

--- test-validationtool.cpp
+++ test-validationtool.cpp
@@ -3,16 +3,22 @@
 #include "test/catch.hpp"
 #include "sensor-validate.h"
 
-TEST_CASE("reports error when soc jumps abruptly") {
+TEST_CASE("reports error when soc jumps abruptly2") {
   double socReadings[] = {0.0, 0.01, 0.5, 0.51};
   int numOfSocReadings = sizeof(socReadings) / sizeof(socReadings[0]);
-  REQUIRE(validateSOCreadings(socReadings, numOfSocReadings,0.05) == 0);
+  REQUIRE(validateSOCreadings(socReadings, numOfSocReadings,0.05) == 1);
 }
 
-TEST_CASE("reports error when current jumps abruptly") {
+TEST_CASE("reports error when current jumps abruptly2") {
   double currentReadings[] = {0.0, 0.02, 0.03, 0.33};
   int numOfCurReadings = sizeof(currentReadings) / sizeof(currentReadings[0]);
-  REQUIRE(validateSOCreadings(currentReadings, numOfCurReadings,0.1) == 0);
+  REQUIRE(validateSOCreadings(currentReadings, numOfCurReadings,0.1) == 1);
+}
+
+TEST_CASE("reports error when soc jumps abruptly3") {
+  double socReadings[] = {0.0, 0.01, 0.02, 0.03};
+  int numOfSocReadings = sizeof(socReadings) / sizeof(socReadings[0]);
+  REQUIRE(validateSOCreadings(socReadings, numOfSocReadings,0.05) == 0);
 }
 
 TEST_CASE("reports error when current readings are not given") {
